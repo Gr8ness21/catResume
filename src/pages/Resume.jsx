8b469819ee95e52a965e633c9e8281544c23b208
Gr8ness21 <pmleechapman@gmail.com>
2026-05-01 11:17:28 -0400

    updated resume page to remove dupe nav bar

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 function Resume() {
     return (
         <>
-            <Navbar />
             <div className="resumeContainer">
                 <h1>Resume Component</h1>
             </div>
