e926f2af85814a076f840b8807eba2f584440ed6
Gr8ness21 <pmleechapman@gmail.com>
2026-05-01 11:08:02 -0400

    Updated nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Resume</li>
+                {/* Scrolls within the same view/page */}
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+
+                {/* Leads to external page or component */}
+                <li><Link to="/resume"> Resume </Link></li>
                 {/* The goal: */}
                 {/* <li><Link to="/resume">Resume</Link></li> */}
             </ul>
