dc9b1153500864c99e223481b2191d6c811d7565
Gr8ness21 <pmleechapman@gmail.com>
2026-05-01 07:25:55 -0400

    Imported all components and added them to the return

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
+import About from './components/About.jsx';
+import ParallaxImage from './components/ParallaxImage';
+import Contact from './components/Contact.jsx';
 
 function App() {
   return (
     <>
     <Navbar/>
     <h1>Cat Resume</h1>
+    <About/>
+    <ParallaxImage/>
+    <Contact/>
     </>
   )
 }
