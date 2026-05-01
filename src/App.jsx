a02b693aa216be96e308c3b9e908e97464c01c0e
Gr8ness21 <pmleechapman@gmail.com>
2026-05-01 10:53:16 -0400

    refactored the strucure of our app to prepare for nav functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import Navbar from './components/Navbar.jsx';
 import About from './components/About.jsx';
 import ParallaxImage from './components/ParallaxImage';
 import Contact from './components/Contact.jsx';
+import Resume from './pages/Resume.jsx'
+
+import { Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+      <h1 id="home">Cat Resume</h1>
+      <About />
+      <ParallaxImage />
+      <Contact />
+    </>
+  )
+}
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <h1>Cat Resume</h1>
-    <About/>
-    <ParallaxImage/>
-    <Contact/>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="resume" element={<Resume />} />
+      </Routes>
+
     </>
   )
 }
