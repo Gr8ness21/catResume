6414429bcaf3297ec3c4ba962a07c109875ff5a8
Gr8ness21 <pmleechapman@gmail.com>
2026-05-01 11:33:15 -0400

    Updated resume component to include a back to home button. updated css to change flex direction.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,16 @@
-import Navbar from "../components/Navbar";
+import { Link } from 'react-router-dom'
 
 function Resume() {
     return (
         <>
             <div className="resumeContainer">
                 <h1>Resume Component</h1>
+                {/* <a href="/">Home</a> */}
+                {/* Odd, but can work ^ */}
+                <Link to="/">Back to Home Page</Link>
             </div>
+
+
         </>
     )
 }
